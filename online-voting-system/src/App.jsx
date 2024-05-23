ef2c100c0ed5b7095e6af36aa0a64a1e2abacf59
aayush598 <aayushgid598@gmail.com>
2024-05-23 14:51:57 +0530

    app.jsx updated

--- online-voting-system/src/App.jsx
+++ online-voting-system/src/App.jsx
@@ -1,31 +1,16 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import SideNavbar from './components/Layout/SideNavbar';
-// import Home from './pages/Home';
-// import Dashboard from './pages/Dashboard';
-// import History from './pages/History';
-// import Results from './pages/Results';
-// import Help from './pages/Help';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SideNavbar from './components/Layout/Sidebar';
+
 
 function App() {
   return (
-    // <Router>
-    //   <div className="flex">
-    //     <SideNavbar />
-    //     <div className="flex-1 p-4 md:ml-64">
-    //       <Switch>
-    //         <Route exact path="/" component={Home} />
-    //         <Route path="/dashboard" component={Dashboard} />
-    //         <Route path="/history" component={History} />
-    //         <Route path="/results" component={Results} />
-    //         <Route path="/help" component={Help} />
-    //       </Switch>
-    //     </div>
-    //   </div>
-    // </Router>
-    <>
-      <h1 className="text-cyan-300">AaSHK</h1>
-    </>
+      <div className="flex">
+        <SideNavbar />
+
+      </div>
+ 
+
   );
 }
 
