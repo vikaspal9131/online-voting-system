6216a22e79d828f5ce6d965515df56274d6beea6
aayush598 <aayushgid598@gmail.com>
2024-05-23 14:34:47 +0530

    Tailwind Initialized

--- online-voting-system/src/App.jsx
+++ online-voting-system/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-
-import './App.css'
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import SideNavbar from './components/Layout/SideNavbar';
+// import Home from './pages/Home';
+// import Dashboard from './pages/Dashboard';
+// import History from './pages/History';
+// import Results from './pages/Results';
+// import Help from './pages/Help';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    // <Router>
+    //   <div className="flex">
+    //     <SideNavbar />
+    //     <div className="flex-1 p-4 md:ml-64">
+    //       <Switch>
+    //         <Route exact path="/" component={Home} />
+    //         <Route path="/dashboard" component={Dashboard} />
+    //         <Route path="/history" component={History} />
+    //         <Route path="/results" component={Results} />
+    //         <Route path="/help" component={Help} />
+    //       </Switch>
+    //     </div>
+    //   </div>
+    // </Router>
     <>
-     <h1>voting system</h1>
+      <h1 className="text-cyan-300">AaSHK</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
